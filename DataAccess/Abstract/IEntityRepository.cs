d6db42d7623069e335b4ab250163524f83752bf6
KamilSahin <kmlshn1352@gmail.com>
2022-08-23 17:45:39 +0300

    08/23/22 day 8 2.09.31

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository
+    // generic constraint -- kısıtlama
+
+    // class -- referans tip olabilir demek. class olabilir demek değil
+
+    // new() -- new'lenebilir olmalı IEntity kullanmamak için , sadece türevlerini kullanmak için
+    public interface IEntityRepository<T> where T : class,IEntity,new()
     {
+        List<T> GetAll(Expression<Func<T,bool>> filter= null); // null ise filtre vermeyebilir demek
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+       
     }
 }
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,12 +1,66 @@
-﻿using System;
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal
+    public class EfProductDal : IProductDal
     {
+        public void Add(Product entity)
+        {
+            // using performans için. garbage olarak atıyor kullandıktan sonra
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var addedEntity = context.Entry(entity); //referansı yakala
+                addedEntity.State = EntityState.Added; // eklenecek nesneyi ekle
+                context.SaveChanges(); // kaydet
+            } 
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity); //referansı yakala
+                deletedEntity.State = EntityState.Deleted; // eklenecek nesneyi ekle
+                context.SaveChanges(); // kaydet
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var updatedEntity = context.Entry(entity); //referansı yakala
+                updatedEntity.State = EntityState.Modified; // eklenecek nesneyi ekle
+                context.SaveChanges(); // kaydet
+            }
+        }
     }
 }
